4888ca2761d82f2f4eb217bd578ed46113faf36b
nd345-tech <ndemetrio345@gmail.com>
2026-02-10 17:43:37 -0500

    Initial commit

---
+++ Content/Items/TutorialSword.cs
@@ -0,0 +1,40 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TutorialMod.Content.Items
+{
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+	public class TutorialSword : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TutorialMod.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.damage = 1000;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 100;
+			Item.height = 1000;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 10;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddIngredient(ItemID.StoneBlock, 1);
+            recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+
+
+	}
+}
